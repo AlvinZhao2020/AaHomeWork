b38b9e3224681db69daea09c86e07b5dff006d32
alvin zhao <yibozhao2018@gmail.com>
2020-02-28 00:33:42 -0800

    build class Stack and class Queue with #methods

---
+++ W3D5/DIY_Adts.rb
@@ -0,0 +1,34 @@
+ class Stack
+    def initialize
+       @stack = []
+    end
+
+    def push(el)
+       @stack << el
+    end
+
+    def pop
+      @stack.pop
+    end
+
+    def peek
+      @stack[-1]
+    end
+  end
+
+
+
+class   Queue
+    def initialize
+        @queue = []
+    end
+    def enqueue(el)
+        @queue.push(el)
+    end
+    def dequeue
+        @queue.shift
+    end
+    def peek
+        @queue[0]
+    end
+end
