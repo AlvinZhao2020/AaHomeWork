1bdf4562a854a9577f5aa09f9520fe540887ef69
alvin zhao <yibozhao2018@gmail.com>
2020-09-18 21:32:44 -0700

    practice js

--- w11d3/frontend/components/session/signup_container.jsx
+++
@@ -1,9 +0,0 @@
-import {connect} from 'react-redux'
-import { createNewUser} from '../../actions/session'
-import SignupForm from './signup'
-
-const mDTP = dispatch =>({
-    createNewUser: formuser=>dispatch(createNewUser(formuser))
-})
-
-export default connect(null,mDTP)(SignupForm)
